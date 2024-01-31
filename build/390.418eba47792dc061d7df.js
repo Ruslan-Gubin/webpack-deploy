6bd8951332bb71300108949ed7052372b66b54d2
ruslan1986 <gubin_ruslan@rambler.ru>
2024-01-31 13:11:07 +0300

    build1

--- build/390.418eba47792dc061d7df.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkwebpack_config=self.webpackChunkwebpack_config||[]).push([[390],{390:(e,c,n)=>{n.r(c),n.d(c,{default:()=>t});var a=n(294);const t=function(){return a.createElement("h1",null,"ShopPage")}}}]);
-//# sourceMappingURL=390.418eba47792dc061d7df.js.map
